fc6704fa811566c6defe436e219450d396225e95
rohit-htc <rohit.anand@hindustantimes.com>
2015-05-02 23:54:14 +0530

    vis version 3 added

--- cprograms/vis.c
+++ cprograms/vis.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
+int strip=0;
 
-main(int argc,char* argv[])
-{
-	int c,strip=0;
-	if (argc>1 && strcmp(argv[1],"-s")==0) {
-		strip=1;
-	}
-	while ((c=getchar()) != EOF) {
+vis(FILE *fp){
+	int c;
+	while ((c=getc(fp)) != EOF) {
 		if (isascii(c) && (isprint(c) || c=='\n' || c=='\t' || c==' ')) {
 			putchar(c);
 		}
@@ -16,5 +13,41 @@ main(int argc,char* argv[])
 
 		}
 	}
-	exit(0);
+	return 0;
+}
+
+main(int argc,char* argv[])
+{
+	int i;
+	FILE *fp;
+	while (argc>1 && argv[1][0]=="-") {
+		switch(argv[1][1]){
+			case 's':
+				strip=1;
+				break;
+			default:
+				fprintf(stderr,"%s:unknown arg %s\n",argv[0],argv[1]);
+				exit(1);
+
+		}
+		argc--;
+		argv++;
+	}
+	if(argc==1){
+		vis(stdin);
+
+	}
+	else{
+		for (i = 1; i < argc; i++) {
+			if ((fp=fopen(argv[i],"r"))==NULL) {
+				fprintf(stderr,"%s: Cannot open %s\n",argv[0],argv[1]);
+			}
+			else{
+				vis(fp);
+				fclose(fp);
+
+			}
+		}
+
+	}
 }
